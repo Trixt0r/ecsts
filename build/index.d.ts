25b7fc47d0aff79ecd7bb84741fca5ad97e61348
Heinrich Reich <h.reich90@gmail.com>
2019-05-21 21:31:20 +0200

    Updated index paths.

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export * from './core/collection';
 export * from './core/component';
 export * from './core/dispatcher';
@@ -7,13 +6,3 @@ export * from './core/entity';
 export * from './core/filter';
 export * from './core/system';
 export * from './core/types';
-=======
-export * from 'core/collection';
-export * from 'core/component';
-export * from 'core/dispatcher';
-export * from 'core/engine';
-export * from 'core/entity';
-export * from 'core/filter';
-export * from 'core/system';
-export * from 'core/types';
->>>>>>> Emit alslo the declaration files.
